fbe335f8da61c3b817d841cb28ac4152217a6234
abanoubmg <abanoubmeshriki@gmail.com>
2025-09-27 11:38:16 +0300

    Fix accessibility enabled detection

--- android/app/src/main/kotlin/com/example/smart_autocorrect/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/smart_autocorrect/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.smart_autocorrect
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
+import android.content.ComponentName
 import android.content.Intent
 import android.provider.Settings
 import android.net.Uri
+import android.text.TextUtils
 
 class MainActivity: FlutterActivity() {
     private val CHANNEL = "smart_autocorrect/accessibility"
@@ -52,13 +54,25 @@ class MainActivity: FlutterActivity() {
             Settings.Secure.ACCESSIBILITY_ENABLED,
             0
         )
-        
+
         if (accessibilityEnabled == 1) {
-            val services = Settings.Secure.getString(
+            val enabledServices = Settings.Secure.getString(
                 contentResolver,
                 Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES
-            )
-            return services?.contains("$packageName/.AutoCorrectAccessibilityService") == true
+            ) ?: return false
+
+            val splitter = TextUtils.SimpleStringSplitter(':')
+            splitter.setString(enabledServices)
+
+            val targetService = ComponentName(this, AutoCorrectAccessibilityService::class.java)
+
+            while (splitter.hasNext()) {
+                val componentName = splitter.next()
+                val enabledComponent = ComponentName.unflattenFromString(componentName)
+                if (enabledComponent != null && enabledComponent == targetService) {
+                    return true
+                }
+            }
         }
         return false
     }
